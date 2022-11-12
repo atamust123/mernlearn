6f4bebed924a296b4a807c2548d0ba4432e75f69
b21526681 <b21526681@cs.hacettepe.edu.tr>
2022-11-12 22:53:02 +0300

    day3 express requests methods done

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { AddReview } from "./components/AddReview";
+import { Restaurant } from "./components/Restaurant";
+import { RestaurantList } from "./components/RestaurantList";
+import { Login } from "./components/Login"
+function App() {
+  return (
+    <div>
+      Atakan
+    </div>
+  );
+}
+
+export default App;
